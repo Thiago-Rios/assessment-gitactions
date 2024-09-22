0f6a6fe4b4e52cc594e271a7db1cceaf65b3cb72
Thiago-Rios <thiago.mgoncalves@al.infnet.edu.br>
2024-09-22 17:43:18 -0300

    Initial commit - Actions

---
+++ src/main/java/com/infnet/assessmentgitactions/AssessmentGitactionsApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.assessmentgitactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentGitactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssessmentGitactionsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/infnet/assessmentgitactions/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.infnet.assessmentgitactions.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Ola povo, estou alterando este codigo!";
+    }
+
+    @GetMapping("/oiAula")
+    public String oiAula() {
+        return "oi povo, vamos testar o nosso github actions";
+    }
+
+    @GetMapping("/tres")
+    public String tres() {
+        return "testando";
+    }
+}
